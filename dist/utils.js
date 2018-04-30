a5c6e4003aa292b2f8f5e0c651ca90b709a47650
Mohamed Shaban <m.shaban@malsapp.com>
2018-04-30 09:51:53 +0200

    ignore dist folder

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var getDisplayName = exports.getDisplayName = function getDisplayName(WrappedComponent) {
-  return WrappedComponent.displayName || WrappedComponent.name || WrappedComponent.type && WrappedComponent.type.displayName;
-};
